beedcb31ce6d1076d79d8f46b27e9578a404d4aa
BRPUBLICATION <brpublishingh@gmail.com>
2025-05-16 00:04:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Animate past event cards on scroll
-function animatePastEvents() {
-  const cards = document.querySelectorAll('.event-card');
 
-  cards.forEach(card => {
-    const rect = card.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-
-    if (rect.top < windowHeight - 100) {
-      card.classList.add('visible');
-    }
-  });
-}
-
-window.addEventListener('scroll', animatePastEvents);
-window.addEventListener('load', animatePastEvents);
